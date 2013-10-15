e9787de370ce163579f694ee3a0d29094c5db8f3
Brian Murray brian.murray@ssec.wisc.edu (Student for D. Hackel) <bmurray@zara.ssec.wisc.edu>
2013-10-15 17:21:49 +0000

    fixed setup.sh to properly backup and replace dotfiles

--- setup.sh
+++ setup.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+# move dotfiles to a backup directory if they already exist
+mkdir ~/dotfiles_backup
+mv ~/.emacs ~/dotfiles_backup/.emacs
+mv ~/.emacs.d ~/dotfiles_backup/.emacs.d
+mv ~/.vim ~/dotfiles_backup/.vim
+mv ~/.vimrc ~/dotfiles_backup/.vimrc
+mv ~/.gvimrc ~/dotfiles_backup/.gvimrc
+
 # setup the home directories dotfiles to point here
-ln -s .emacs ~/.emacs
-ln -s .emacs.d ~/.emacs.d
-ln -s .vim ~/.vim
-ln -s .vimrc ~/.vimrc
-ln -s .gvimrc ~/.gvimrc
+ln -s $(pwd)/.emacs ~/.emacs
+ln -s $(pwd)/.emacs.d ~/.emacs.d
+ln -s $(pwd)/.vim ~/.vim
+ln -s $(pwd)/.vimrc ~/.vimrc
+ln -s $(pwd)/.gvimrc ~/.gvimrc
