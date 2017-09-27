bc9109d45791bc3ef51be4d6e9fa35cdb82757ce
Brian Murray <brianmurray@brianmurray.mtv.corp.google.com>
2017-09-27 02:08:13 -0700

    Add hgrc/hgignore and update setup.sh.

--- setup.sh
+++ setup.sh
@@ -6,6 +6,8 @@ mv ~/.bashrc ~/dotfiles_backup/.bashrc
 mv ~/.emacs ~/dotfiles_backup/.emacs
 mv ~/.emacs.d ~/dotfiles_backup/.emacs.d
 mv ~/.gvimrc ~/dotfiles_backup/.gvimrc
+mv ~/.hgrc ~/dotfiles_backup/.hgrc
+mv ~/.hgignore ~/dotfiles_backup/.hgignore
 mv ~/.jartps1.sh ~/dotfiles_backup/.jartps1.sh
 mv ~/.tmux.conf ~/dotfiles_backup/.tmux.conf
 mv ~/.vim ~/dotfiles_backup/.vim
@@ -16,6 +18,8 @@ ln -s $(pwd)/.bashrc ~/.bashrc
 ln -s $(pwd)/.emacs ~/.emacs
 ln -s $(pwd)/.emacs.d ~/.emacs.d
 ln -s $(pwd)/.gvimrc ~/.gvimrc
+ln -s $(pwd)/.hgrc ~/.gvimrc
+ln -s $(pwd)/.hgignore ~/.hgignore
 ln -s $(pwd)/.jartps1.sh ~/.jartps1.sh
 ln -s $(pwd)/.tmux.conf ~/.tmux.conf
 ln -s $(pwd)/.vim ~/.vim
