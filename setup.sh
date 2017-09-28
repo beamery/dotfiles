54c283b80fc0795c519ac7fe3ebd70ca443de14d
Brian Murray <brianmurray@brianmurray.mtv.corp.google.com>
2017-09-28 01:43:58 -0700

    Added .g4d

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ mkdir ~/dotfiles_backup
 mv ~/.bashrc ~/dotfiles_backup/.bashrc
 mv ~/.emacs ~/dotfiles_backup/.emacs
 mv ~/.emacs.d ~/dotfiles_backup/.emacs.d
+mv ~/.g4d ~/dotfiles_backup/.g4d
 mv ~/.gvimrc ~/dotfiles_backup/.gvimrc
 mv ~/.hgrc ~/dotfiles_backup/.hgrc
 mv ~/.hgignore ~/dotfiles_backup/.hgignore
@@ -17,6 +18,7 @@ mv ~/.vimrc ~/dotfiles_backup/.vimrc
 ln -s $(pwd)/.bashrc ~/.bashrc
 ln -s $(pwd)/.emacs ~/.emacs
 ln -s $(pwd)/.emacs.d ~/.emacs.d
+ln -s $(pwd)/.g4d ~/.g4d
 ln -s $(pwd)/.gvimrc ~/.gvimrc
 ln -s $(pwd)/.hgrc ~/.hgrc
 ln -s $(pwd)/.hgignore ~/.hgignore
